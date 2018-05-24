340882c07654117384e7c72b8dcb045ac87b2fbc
Winston Wang <winstonwang12@gmail.com>
2018-05-24 16:20:33 -0700

    started checkpoint saving

--- main.py
+++ main.py
@@ -8,13 +8,40 @@ import torchvision.datasets as dset
 import torchvision.transforms as T
 
 import numpy as np
+import argparse
 
 from util.dataset_class import MammogramDataset
+from util.checkpoint import save_model, load_model
 
-# PARAMETERS
-print_every = 1 # Constant to control how frequently we print train loss
-USE_GPU = True #Use GPU if available
-BATCH_SIZE = 10
+# ARGS
+parser = argparse.ArgumentParser()
+parser.add_argument("--debug", action='store_true', help="Debug mode: ???")
+parser.add_argument("--load_path", help="optional path argument, if we want to load an existing model")
+parser.add_argument("--print_every", default = 100, type=int, help="print loss every this many iterations")
+parser.add_argument("--use_cpu", action='store_true', help="Use GPU if possible, set to false to use CPU")
+parser.add_argument("--batch_size", default = 10, type=int, help="Batch size")
+parser.add_argument("--val_ratio", default = 0.2, type=float, help="Proportion of training data set aside as validation")
+parser.add_argument("--mode", help="can be train, test, or vis")
+parser.add_argument("--save_every", default = 10, type=int, help="save model at this this many epochs")
+parser.add_argument("--val_every", default = 1, type=int, help="validate model at this many epochs")
+args = parser.parse_args()
+
+#Setup
+debug = args.debug
+load_path = args.load_path
+print_every = args.print_every
+USE_GPU = False if args.use_cpu else True
+VAL_RATIO = args.val_ratio
+mode = args.mode
+save_every = args.save_every
+val_every = args.val_every
+
+#Hyperparameters
+BATCH_SIZE = args.batch_size
+
+#Make sure parameters are valid
+assert mode == 'test' or mode == 'train' or mode == 'vis'
+if mode == 'vis': assert load_path is not None
 
 # The torchvision.transforms package provides tools for preprocessing data
 # and for performing data augmentation; here we set up a transform to
@@ -29,7 +56,6 @@ train_data = MammogramDataset("data", "train")#Can add transform = transform as 
 val_data = MammogramDataset("data", "train")
 test_data = MammogramDataset("data", "test")
 
-VAL_RATIO = 0.2
 NUM_VAL = int(len(train_data)*VAL_RATIO)
 NUM_TRAIN = len(train_data) - NUM_VAL
 NUM_TEST = len(test_data)
@@ -46,16 +72,35 @@ else:
     device = torch.device('cpu')
 print('using device:', device)
 
-for t, sample in enumerate(loader_train):
-    # Move the data to the proper device (GPU or CPU)
-    x = sample['image']
-    y = sample['label']
-    x = x.to(device=device, dtype=dtype)
-    y = y.to(device=device, dtype=torch.long)
+def train(model_fn, params, learning_rate):
+    epoch = 1
+    while True:
+        for t, sample in enumerate(loader_train):
+            x = sample['image']
+            y = sample['label']
+            # Move the data to the proper device (GPU or CPU)
+            x = x.to(device=device, dtype=dtype)
+            y = y.to(device=device, dtype=torch.long)
 
-    if t % print_every == 0:
-        print('Iteration %d' % (t))
-        print('y = ', y.shape)
-        print('x shape = ', x.shape)
-        print()
+            # Forward pass: compute scores and loss
+            scores = model_fn(x, params)
+            loss = F.cross_entropy(scores, y)
+            loss.backward()
+
+            with torch.no_grad():
+                for w in params:
+                    w -= learning_rate * w.grad
+                    w.grad.zero_()
+
+            if t % print_every == 0:
+                print('Iteration %d' % (t))
+                print('y = ', y.shape)
+                print('x shape = ', x.shape)
+                print()
+        if epoch % save_every == 0:
+            save_model() #NEEDS WORK
+        if epoch % val_every == 0:
+            val_model() #NEEDS WORK
+        epoch += 1
+        
 
---
+++ util/checkpoint.py
@@ -0,0 +1,22 @@
+'''
+Saves provided model as the latest model.  If the model performed better
+than the current best model, then replace the current best model as well
+'''
+def save_model():
+    #Overwrite latest model
+    #Compare model statistics with best model statistics
+    #If model statistics are better
+        #Replace model and statistics
+    pass
+
+'''
+Loads the model at the given path
+'''
+def load_model():
+    pass
+
+'''
+Turns a model and its parameters into a file
+'''
+def write_model():
+    pass
