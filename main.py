777439dfbb4f2f086a753c4b55363176c17306c6
Winston Wang <winstonwang12@gmail.com>
2018-05-27 00:12:25 -0700

    wrote a quick script to calculate mean and std of training data

--- main.py
+++ main.py
@@ -16,32 +16,30 @@ from util.checkpoint import save_model, load_model
 # ARGS
 parser = argparse.ArgumentParser()
 parser.add_argument("--debug", action='store_true', help="Debug mode: ???")
-parser.add_argument("--load_path", help="optional path argument, if we want to load an existing model")
+parser.add_argument("--checkpoint", help="optional path argument, if we want to load an existing model")
 parser.add_argument("--print_every", default = 100, type=int, help="print loss every this many iterations")
 parser.add_argument("--use_cpu", action='store_true', help="Use GPU if possible, set to false to use CPU")
 parser.add_argument("--batch_size", default = 10, type=int, help="Batch size")
 parser.add_argument("--val_ratio", default = 0.2, type=float, help="Proportion of training data set aside as validation")
 parser.add_argument("--mode", help="can be train, test, or vis")
 parser.add_argument("--save_every", default = 10, type=int, help="save model at this this many epochs")
-parser.add_argument("--val_every", default = 1, type=int, help="validate model at this many epochs")
 args = parser.parse_args()
 
 #Setup
 debug = args.debug
-load_path = args.load_path
+checkpoint = args.checkpoint
 print_every = args.print_every
 USE_GPU = False if args.use_cpu else True
 VAL_RATIO = args.val_ratio
 mode = args.mode
 save_every = args.save_every
-val_every = args.val_every
 
 #Hyperparameters
 BATCH_SIZE = args.batch_size
 
 #Make sure parameters are valid
 assert mode == 'test' or mode == 'train' or mode == 'vis'
-if mode == 'vis': assert load_path is not None
+if mode == 'vis': assert checkpoint is not None
 
 # The torchvision.transforms package provides tools for preprocessing data
 # and for performing data augmentation; here we set up a transform to
@@ -72,8 +70,14 @@ else:
     device = torch.device('cpu')
 print('using device:', device)
 
-def train(model_fn, params, learning_rate):
+'''
+Take a loader, model, and optimizer.  Use the optimizer to update the model
+based on the training data, which is from the loader.  Does not terminate,
+saves best checkpoint and latest checkpoint
+'''
+def train(loader, model, optimizer):
     epoch = 1
+    model = model.to(device=device)
     while True:
         for t, sample in enumerate(loader_train):
             x = sample['image']
@@ -82,28 +86,40 @@ def train(model_fn, params, learning_rate):
             x = x.to(device=device, dtype=dtype)
             y = y.to(device=device, dtype=torch.long)
 
-            # Forward pass: compute scores and loss
-            scores = model_fn(x, params)
+            scores = model(x)
             loss = F.cross_entropy(scores, y)
+            optimizer.zero_grad()
             loss.backward()
-
-            with torch.no_grad():
-                for w in params:
-                    w -= learning_rate * w.grad
-                    w.grad.zero_()
+            optimizer.step()
 
             if t % print_every == 0:
                 print('Iteration %d' % (t))
                 print('y = ', y.shape)
                 print('x shape = ', x.shape)
                 print()
+        acc = check_accuracy(loader_val, model_fn, params)
         if epoch % save_every == 0:
             save_model() #NEEDS WORK
-        if epoch % val_every == 0:
-            val_model() #NEEDS WORK
         epoch += 1
         
-def test(model_fn, params):
-    
+'''
+Takes a data loader and a model, then returns the model's accuracy on
+the data loader's data set
+'''
+def check_accuracy(loader, model):
+    num_correct, num_samples = 0, 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for sample in loader:
+            x = sample['image']
+            y = sample['label']
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+    return acc
         
 
