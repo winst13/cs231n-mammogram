43f0f857ed6fdda3765a7d24d7c86e0f23a288f1
Owen Wang <ojwang@stanford.edu>
2018-06-05 00:11:26 -0700

    testing saliency with sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,17 +1,30 @@
 import numpy as np
 import torch
 
+from os.path import join
+
 # Project imports
 from model.mammogram_densenet import MammogramDenseNet
-from model.helper import *
-from util.image import normalize_between, saliency2imgarray
+from model import helper
+from util.image import normalize_between, create_saliency_overlay
+from util.checkpoint import load_model
 
 
 if __name__ == "__main__":
-    # testarr = np.random.rand(2,1024,1024)
-    # normed = normalize_between(testarr, 10, 11, batch=True)
-    # print(normed)
+    model = helper.get_reduced_densenet()
+    # This is a dummy optimizer
+    optimizer = optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3, betas = (0.9, 0.999), weight_decay=0)
+    load_model('dense_060518', model, optimizer, mode='best') # rehydrate model
+    del optimizer # goodbye
 
+    filename = "Mass-Test_P_01617_LEFT_CC.npy"
+    imagepath = join("data/mass-test-labeled/1", filename)
+    savepath = join("visualize_output", filename[:-4] + "_saliency.png")
+    create_saliency_overlay(model, imagepath, savepath)
+
+
+    """
     model = get_nopretrain_densenet(debug=True)
     test = model(torch.rand(1,1,1024,1024))
     print(model)
+    """
