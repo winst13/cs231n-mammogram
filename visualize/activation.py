ead9a2124a2503aecf988bcd4a10b668343a8e02
Winston Wang <winstonwang12@gmail.com>
2018-06-07 04:10:31 -0700

    bug

--- visualize/activation.py
+++ visualize/activation.py
@@ -21,20 +21,20 @@ def get_activation(model, layer, image, device = torch.device('cuda'), dtype = t
     
     for module_pos, module in model.features._modules.items():
         if "denseblock" in module_pos:
-            for module_pos_1, module_1 in module.modules():
+            for module_pos_1, module_1 in module.named_modules():
                 print (module_pos_1, module_1)
                 if layer in module_pos_1:
                     x = module_1(x)
                     conv_output.append(x)
                 elif "denselayer" in module_pos_1:
-                    for module_pos_2, module_2 in module_1.modules():
+                    for module_pos_2, module_2 in module_1.named_modules():
                         if layer in module_pos_2:
                             x = module_2(x)
                             conv_output.append(x)
                         else:
                             x = module_2(x)
                 else:
-                    x = module_pos_1(x)
+                    x = module_1(x)
         elif layer in module_pos:
             x = module(x)
             conv_output.append(x)  # Save the convolution output on that layer
